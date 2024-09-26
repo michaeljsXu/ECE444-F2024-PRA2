d0cfcc258d6abaa0905c6558664381d457da3d1a
Michael Xu <michaelx.0277@gmail.com>
2024-09-25 22:17:14 -0400

    added flash for name and email change, done 1.4

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
@@ -15,12 +15,15 @@ moment = Moment(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
-    email = None
     form = MainForm()
     if form.validate_on_submit():
-        name = form.name.data
-        email = form.uoft_email.data
-        form.name.data = ''
-        form.uoft_email.data = ''
-    return render_template('index.html', name=name, current_time=datetime.utcnow(), form=form)
+        old_name = session.get('name')
+        old_email = session.get('email')
+        if (old_name is not None and old_name != form.name.data):
+            flash('Looks like you have changed your name!')
+        if (old_email is not None and old_email != form.uoft_email.data):
+            flash('Looks like you have changed your email!')
+        session['name'] = form.name.data
+        session['email'] = form.uoft_email.data
+        return redirect(url_for('index'))
+    return render_template('index.html', name=session.get('name'), form=form)
