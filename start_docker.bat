ca6c12fd896d037fbbb9631a303f19e6867e3230
Michael Xu <michaelx.0277@gmail.com>
2024-09-26 11:15:47 -0400

    dockerized the application

---
+++ start_docker.bat
@@ -0,0 +1,9 @@
+@echo off
+rem Run the command and capture the output
+set OUTPUT=
+for /f "tokens=*" %%i in ('"C:\Program Files\Docker\Docker\resources\bin\docker" run -d --env-file .env -p 5000:5000 ece444f2024pra2') do set OUTPUT=%%i
+
+rem Write the captured output to a file
+echo docker kill %OUTPUT% > docker_kill.bat
+
+pause
